ca4613a1cb7dc3bbfa27b33411ffb6411ca028b6
valedithat <valedithat@pm.me>
2022-09-29 18:05:29 -0400

    create initial Users table, add to apps

--- rnd/settings.py
+++ rnd/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local APPS
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nick_name = models.CharField(max_length=254)
+    host_name = models.CharField(max_length=254)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
