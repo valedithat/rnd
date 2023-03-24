1a60f8efc061268c23b16509f168cb1730c7c021
valedithat <valedithat@pm.me>
2023-03-24 01:43:29 -0400

    add articles to admin and urls

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.http import HttpResponse
+
+def articles(request):
+    return HttpResponse("Articles and Shit")
